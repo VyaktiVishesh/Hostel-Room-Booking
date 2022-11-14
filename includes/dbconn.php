d104455e11ec52bb8935d0dfd09ff64bfa40aa39
VyaktiVishesh <108505308+VyaktiVishesh@users.noreply.github.com>
2022-11-15 03:19:34 +0530

    Update dbconn.php

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -1,7 +1,7 @@
+
 <?php
-    $dbuser="root";
-    $dbpass="";
-    $host="localhost";
-    $db="project";
-    $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+  $mysqli = mysqli_init();
+  $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+  $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
+  $mysqli->close();
 ?>
